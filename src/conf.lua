59c9eb0c399ecd625f2cd723ec2d2a88903adc55
Jack Riales <jack@thatnaughtypanda.com>
2016-08-07 23:46:29 -0500

    Added configuration file

---
+++ src/conf.lua
@@ -0,0 +1,12 @@
+-- LOVE Configuration file
+
+function love.conf(t)
+   t.identity = "Anniversary9"
+   t.accelerometerjoystick = false
+
+   t.window.title = "Cyan's Quest 2"
+   t.window.icon = nil -- TODO(Jack)
+   t.window.width = 1280
+   t.window.height = 720
+   t.window.fullscreen = false -- TODO(Jack): When done, maybe set to true
+end
