603b4df1c6ff3e0cfeb52f9652670fe62d6f22d0
Jack Riales <jack@thatnaughtypanda.com>
2016-08-09 15:57:17 -0500

    Added player data

---
+++ game/data/Player-Cyan.lua
@@ -0,0 +1,39 @@
+--[[ Copyright (c) 2016 Jack Riales. All Rights Reserved.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+return {
+   -- General info
+   name   = "Cyan",
+   color  = { r = 0, g = 255, b = 255 },
+
+   -- Movement
+   speed  = 80,
+   accel  = 10,
+
+   -- Shot
+   
+   
+   -- Sprite definition
+   sprite = "data.Sprite-Cyan",
+
+   -- Animation names
+   states = {
+      idle = "dance",
+      move_left = nil, -- TODO
+      move_right= nil  -- TODO
+      die       = nil  -- TODO
+   }
+}
