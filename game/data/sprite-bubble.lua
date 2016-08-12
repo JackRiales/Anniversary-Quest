2368e5f6fea9123d4ba452d17bb0df531c37a249
Jack Riales <jack@thatnaughtypanda.com>
2016-08-11 23:54:54 -0500

    Added bubble sprite with data file

---
+++ game/data/sprite-bubble.lua
@@ -0,0 +1,40 @@
+--[[ Copyright (c) 2016 Jack Riales. All Rights Reserved.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+require "love.graphics"
+
+local texture_width = 128
+local texture_height= 16
+local sprite_tile_size = 16
+
+return {
+   texture_url = "assets/spr/SprBubble.png",
+   sprite_name = "Bubble",
+   frame_duration = 0.10,
+   animation_names = {"default"},
+   animations = {
+      default = {
+	 love.graphics.newQuad(0,  0, sprite_tile_size, sprite_tile_size, texture_width, texture_height),
+	 love.graphics.newQuad(16, 0, sprite_tile_size, sprite_tile_size, texture_width, texture_height),
+	 love.graphics.newQuad(32, 0, sprite_tile_size, sprite_tile_size, texture_width, texture_height),
+	 love.graphics.newQuad(48, 0, sprite_tile_size, sprite_tile_size, texture_width, texture_height),
+	 love.graphics.newQuad(64, 0, sprite_tile_size, sprite_tile_size, texture_width, texture_height),
+	 love.graphics.newQuad(96, 0, sprite_tile_size, sprite_tile_size, texture_width, texture_height),
+	 love.graphics.newQuad(112,0, sprite_tile_size, sprite_tile_size, texture_width, texture_height),
+	 love.graphics.newQuad(128,0, sprite_tile_size, sprite_tile_size, texture_width, texture_height)
+      }
+   }
+}
