ff367eabaa2c63db952450ca7061c2059aeb7154
Jack Riales <jack@thatnaughtypanda.com>
2016-08-17 00:14:47 -0500

    Removed bubble shooting

--- game/player.lua
+++ game/player.lua
@@ -87,13 +87,6 @@ function Player.new(def)
    self.collider.rect = Collider.new(self.entity:GetPosition().x-self.collider.width/2,
 				     self.entity:GetPosition().y-self.collider.height/2,
 				     self.collider.width, self.collider.height)
-
-   self.bullets = {}
-   self.shot = self.data.shot
-   self.shot.sprite = Sprite.new(self.data.shot.sprite)
-   self.shot.direction = Vec2.new(0, 1)
-   self.shot.time = 0
-   self.shooting = false
    
    return setmetatable(self, Player)
 end
@@ -103,10 +96,6 @@ function Player:SetKeyboardControls(controls)
    self.controls.keys.moveDown = controls.moveDown or "s"
    self.controls.keys.moveLeft = controls.moveLeft or "a"
    self.controls.keys.moveRight= controls.moveRight or "d"
-   self.controls.keys.shootUp  = controls.shootUp or "up"
-   self.controls.keys.shootDown= controls.shootDown or "down"
-   self.controls.keys.shootLeft= controls.shootLeft or "left"
-   self.controls.keys.shootRight = controls.shootRight or "right"
 end
 
 function Player:GetMotionInput()
@@ -133,18 +122,6 @@ function Player:GetMotionInput()
       if     string.find(hat, self.controls.joystick.moveLeft)  then Player.Axes.x = -1
       elseif string.find(hat, self.controls.joystick.moveRight) then Player.Axes.x =  1 end
    end
-end
-
-function Player:GetShootInput()
-   if     love.keyboard.isDown(self.controls.keys.shootUp)    then
-      self.shot.direction = Vec2.new(0,-1) ; self.shooting = true
-   elseif love.keyboard.isDown(self.controls.keys.shootDown)  then
-      self.shot.direction = Vec2.new(0,1) ; self.shooting = true
-   elseif love.keyboard.isDown(self.controls.keys.shootLeft)  then
-      self.shot.direction = Vec2.new(-1,0) ; self.shooting = true
-   elseif love.keyboard.isDown(self.controls.keys.shootRight) then
-      self.shot.direction = Vec2.new(1,0) ; self.shooting = true
-   else self.shot.direction = Vec2.new() ; self.shooting = false end
 end
 
 -- Sets the movement vector based on the given vector (input axes)
@@ -191,7 +168,6 @@ end
 function Player:Update(dt)
    -- Gather input
    self:GetMotionInput()
-   self:GetShootInput()
 
    -- Set collider
    self.collider.rect:Set(self.entity:GetPosition().x-self.collider.width/2,
@@ -201,32 +177,6 @@ function Player:Update(dt)
    if #collisions > 1 then
       -- TODO(Jack)
    end
-
-   -- Shooting
-   self.shot.time = self.shot.time + dt
-   if self.shooting then
-      if self.shot.time > self.shot.rate then
-	 -- TODO(Jack): Add pooling; move to class
-	 local bullet = {}
-	 bullet.time = 0
-	 bullet.entity = Entity.new("bullet")
-	 bullet.entity:SetOrigin(8,8)
-	 bullet.entity:SetPosition(self.entity:GetPosition().x+self.shot.direction.x*5, self.entity:GetPosition().y-16)
-	 local bvel = Vec2.scale(Vec2.normalized(self.shot.direction), self.shot.speed)
-	 bullet.entity:SetVelocity(bvel.x,bvel.y)
-	 bullet.entity:SetAcceleration(-bvel.x/2, -bvel.y/2)
-
-	 bullet.sprite = self.shot.sprite
-	 table.insert(self.bullets, bullet)
-	 self.shot.time = 0
-      end
-   end
-   for i,value in ipairs(self.bullets) do
-      value.time = value.time + dt
-      if value.time > self.shot.ttl then table.remove(self.bullets, i) end
-      value.entity:Update(dt)
-   end
-   self.shot.sprite:Update(dt)
 
    -- Move and animate
    self:SetMove(Player.Axes)
@@ -250,11 +200,6 @@ function Player:Draw()
 
    -- Draw Sprite
    self.sprite:Draw(self.entity.transform)
-
-   -- Draw bullets
-   for i,value in ipairs(self.bullets) do
-      value.sprite:Draw(value.entity.transform)
-   end
 end
 
 -- Player debug draw event
@@ -286,11 +231,7 @@ function Player:DrawDebug(color)
    end
    PrintWrapped(wp.x-16, wp.y+10, 10, {self.name,
 				    "P:"..Vec2.toString(self.entity:GetPosition()),
-				    "V:"..Vec2.toString(self.entity:GetVelocity()),
-				    "Shot:"..Vec2.toString(self.shot.direction).." "..tostring(self.shooting)})
-   for i,value in ipairs(self.bullets) do
-      love.graphics.circle("fill", value.entity:GetPosition().x,value.entity:GetPosition().y,3,3)
-   end
+				    "V:"..Vec2.toString(self.entity:GetVelocity())})
    love.graphics.setColor(255,255,255)
 end
 
