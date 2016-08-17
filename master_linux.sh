5e517c313668a98596aa151216d82f2d4f3fa385
Jack Riales <rialesj@gmail.com>
2016-08-16 21:32:29 -0500

    Mastering now works on linux

--- master_linux.sh
+++ master_linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NAME="Anniversary Quest.love"
+NAME="AnniversaryQuest.love"
 
 mkdir -p build/linux
 cd game
-zip -9 -r "tmp.game.love"
-mv tmp.game.love ../build/linux/$NAME .
+zip -9 -r "tmp.game.love" .
+mv tmp.game.love ../build/linux/$NAME
 cd ..
