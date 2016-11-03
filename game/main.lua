c861def2ea20abd2b6c95784301fa2b8b1d348fd
Jack Riales <rialesj@gmail.com>
2016-11-03 13:16:17 -0500

    Modularized debug and camera in main

--- game/main.lua
+++ game/main.lua
@@ -1,15 +1,15 @@
 --[[ Copyright (c) 2016 Jack Riales. All Rights Reserved.
-   
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
@@ -22,15 +22,15 @@
 -- Anyway, I hope you enjoy.
 
 -- Dependencies
-require 'base.camera'
 local GLOBAL = require 'global'
+local Camera = require 'base.camera'
 local Button = require 'base.button'
 local Map    = require 'base.map'
 local Player = require 'player'
 
 -- Require this module for a debug build
 -- Remove on release
-require 'base.debug'
+local Debug = require 'base.debug'
 
 -- Live code editing library
 -- Debug only
