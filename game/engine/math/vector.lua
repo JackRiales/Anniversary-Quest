3a3895c91f67d76759cf0b6fe143ad1fa38efab9
Jack Riales <rialesj@gmail.com>
2017-03-04 00:27:55 -0600

    Objectified vector module

--- game/engine/math/vector.lua
+++ game/engine/math/vector.lua
@@ -18,101 +18,98 @@ local Vec2 = {}
 Vec2.__index = Vec2
 
 function Vec2.new(x, y)
-   return {x=x or 0, y=y or 0}
+   return setmetatable({x=x or 0, y=y or 0}, Vec2)
+end
+
+function Vec2.__call(this, x, y)
+   return Vec2.new(x, y)
+end
+
+function Vec2.__tostring(v)
+   return string.format("(%.2f, %.2f)", v.x, v.y)
+end
+
+function Vec2.__eq(v1, v2)
+   return v1.x == v2.x and v1.y == v2.y
+end
+
+function Vec2.__add(v1, v2)
+   return Vec2(v1.x + v2.x, v1.y + v2.y)
+end
+
+function Vec2.__sub(v1, v2)
+   return Vec2(v1.x - v2.x, v1.y - v2.y)
 end
 
 function Vec2.verify(v)
    return type(v) == 'table' and type(v.x) == 'number' and type(v.y) == 'number'
 end
 
-function Vec2.toString(v)
-   return string.format("(%.2f, %.2f)", v.x, v.y)
-end
-
-function Vec2.dist(v1, v2)
-   assert(Vec2.verify(v1) and Vec2.verify(v2), "Vec2.dist - Wrong argument types")
-   local dx = v1.x - v2.x
-   local dy = v1.y - v2.y
-   return math.sqrt(dx*dx + dy*dy)
-end
-
-function Vec2.dist2(v1, v2)
-   assert(Vec2.verify(v1) and Vec2.verify(v2), "Vec2.dist2 - Wrong argument types")
+function Vec2:dist2(v)
+   assert(Vec2.verify(v), 'Vec2 distance - Wrong argument type')
    local dx = v1.x - v2.x
    local dy = v1.y - v2.y
    return dx*dx + dy*dy
 end
 
-function Vec2.magnitude(v)
-   assert(Vec2.verify(v), "Vec2.magnitude - Wrong argument types")
-   return math.sqrt(v.x*v.x + v.y*v.y)
+function Vec2:dist(v)
+   assert(Vec2.verify(v), 'Vec2 distance - Wrong argument type')
+   return math.sqrt(self:dist2(v))
 end
 
-function Vec2.magnitude2(v)
-   assert(Vec2.verify(v), "Vec2.magnitude2 - Wrong argument types")
-   return v.x*v.x + v.y*v.y
+function Vec2:mag2(v)
+   return self.x * self.x + self.y * self.y
 end
 
-function Vec2.normalized(v)
-   assert(Vec2.verify(v), "Vec2.normalized - Wrong argument types")
-   local len = Vec2.magnitude(v)
+function Vec2:mag()
+   return math.sqrt(self:mag2())
+end
+
+function Vec2:normalized()
+   local len = self:mag()
    if len > 0 then
-      return Vec2.new(v.x/len, v.y/len)
+      return Vec2(self.x/len, self.y/len)
    else
-      return Vec2.new()
+      return Vec2()
    end
 end
 
-function Vec2.floor(v)
-   assert(Vec2.verify(v), "Vec2.floor - Wrong argument types")
-   return Vec2.new(math.floor(v.x), math.floor(v.y))
+function Vec2:floor()
+   return Vec2(math.floor(self.x), math.floor(self.y))
 end
 
-function Vec2.round(v)
-   assert(Vec2.verify(v), "Vec2.round - Wrong argument types")
-   return Vec2.new(math.floor(v.x+0.5), math.floor(v.y+0.5))
+function Vec2:round()
+   return Vec2(math.floor(self.x+0.5), math.floor(self.y+0.5))
 end
 
-function Vec2.trim(v, len)
-   assert(Vec2.verify(v), "Vec2.trim - Wrong argument types")
-   local s = len*len / Vec2.magnitude2(v)
+function Vec2:trim(len)
+   local s = len*len / self:mag2()
    s = (s > 1 and 1) or math.sqrt(s)
-   return Vec2.new(v.x * s, v.y * s)
+   return Vec2(self.x * s, self.y * s)
 end
 
-function Vec2.threshold(v, min)
-   assert(Vec2.verify(v), "Vec2.threshold - Wrong argument types")
-   local len = Vec2.magnitude(v)
+function Vec2:threshold(min)
+   local len = self:mag(v)
    if len < min then
-      return Vec2.new()
+      return Vec2()
    else
-      return v
+      return self
    end
 end
 
-function Vec2.angle(v1, v2)
-   assert(Vec2.verify(v1) and Vec2.verify(v2), "Vec2.angle - Wrong argument types")
-   return math.atan2(v1.y, v1.x) - math.atan2(v2.y, v2.x)
+function Vec2:angle(v)
+   assert(Vec2.verify(v), "Vec2.angle - Wrong argument types")
+   return math.atan2(self.y, self.x) - math.atan2(v.y, v.x)
 end
 
-function Vec2.add(v1, v2)
-   assert(Vec2.verify(v1) and Vec2.verify(v2), "Vec2.add - Wrong argument types")
-   return Vec2.new(v1.x+v2.x, v1.y+v2.y)
+function Vec2:scale(s)
+   assert(type(s) == 'number', "Vec2.scale - Wrong argument types")
+   return Vec2(self.x*s, self.y*s)
 end
 
-function Vec2.sub(v1, v2)
-   assert(Vec2.verify(v1) and Vec2.verify(v2), "Vec2.sub - Wrong argument types")
-   return Vec2.new(v1.x-v2.x, v1.y-v2.y)
-end
-
-function Vec2.scale(v, s)
-   assert(Vec2.verify(v) and type(s) == 'number', "Vec2.scale - Wrong argument types")
-   return Vec2.new(v.x*s, v.y*s)
-end
-
-function Vec2.cross(v1, v2)
-   assert(Vec2.verify(v1) and Vec2.verify(v2), "Vec2.sub - Wrong argument types")
-   return v1.x * v2.y - v1.y * v2.x
+function Vec2:cross(v)
+   assert(Vec2.verify(v2), "Vec2.sub - Wrong argument types")
+   return self.x * v.y - self.y * v.x
 end
 
 return Vec2
